35e91436f2c72865eb63eaf38a6610528f93c40d
juandeoza <jdeoza@gmail.com>
2023-06-05 18:22:28 +0200

    crear la funcionalidad de cambiar el nompre en profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,14 @@
-import { getAuth } from 'firebase/auth';
+import { getAuth, updateProfile } from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
+import { toast } from 'react-toastify';
+import { db } from '../firebase';
 
 export default function Profile() {
   const auth = getAuth();
   const navigate = useNavigate();
+  const [editable, setEditable] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -17,6 +21,31 @@ export default function Profile() {
     navigate('/')
   }
 
+  function onChange(e) {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }))
+  }
+
+  async function onSubmit() {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        
+        await updateProfile( auth.currentUser , {
+          displayName: name
+        } );
+       await setDoc( doc( db , "users" , auth.currentUser.uid ) ,
+       {
+        name: name
+       } );
+       toast.success('se ha actualizado el nombre');
+      }
+    } catch (error) {
+      toast.error('No se pudo actualizar');
+    }
+  }
+
   return (
     <>
       <section className=" max-w-6xl mx-auto flex justify-center  items-center flex-col ">
@@ -25,13 +54,15 @@ export default function Profile() {
           <form>
             {/* nombre input */}
             <input
+              onChange={onChange}
               type="text"
               name=""
               id="name"
               value={name}
-              disabled
-              className=" mb-6 w-full px-4 py-2 text-xl text-gray-700  bg-white
-            border border-gray-300 rounded transition ease-in-out "
+              disabled={!editable}
+
+              className={` mb-6 w-full px-4 py-2 text-xl text-gray-700  bg-white
+            border border-gray-300 rounded transition ease-in-out ${editable && "bg-red-200 focus:bg-red-400"}  `}
             />
             {/* email input */}
 
@@ -47,10 +78,12 @@ export default function Profile() {
 
             <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb6">
               <p className="flex items-center ">
-                ¿Quiere cambiar su nombre ?{' '}
-                <span className="text-red-600  hover:text-green-700 transition duration-200 ease-in-out ml-2 cursor-pointer">
-                  Edit
-                </span>{' '}
+                ¿Quiere cambiar su nombre ?
+                <span
+                  onClick={() => { editable && onSubmit(); setEditable((prev) => !prev) }}
+                  className="text-red-600  hover:text-green-700 transition duration-200 ease-in-out ml-2 cursor-pointer">
+                  {editable ? 'Aplicar cambios' : 'Editar'}
+                </span>
               </p>
               <p
                 onClick={onLogAuth}
